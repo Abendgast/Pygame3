ecb078007fe4d21a78650a48d2d927d95815d6e4
Abendgast <100733158+Abendgast@users.noreply.github.com>
2024-08-20 21:10:22 +0300

    minor changes 21:10

--- src/player.py
+++ src/player.py
@@ -1,27 +1,15 @@
 import pygame
 
-from src.colors import colorSAND, colorRED
-
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
-
-        # Радіус кулі
-        radius = 8  # Пів ширини/висоти поверхні
-
-        # Створення поверхні з прозорим фоном для кулі
-        self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
-
-        # Малювання кулі
-        pygame.draw.circle(self.image, colorRED, (radius, radius), radius)
-
-        # Створення прямокутника для визначення позиції кулі
+        self.image = pygame.Surface((16, 32))
+        self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect(center=position)
-        self.speed = 200  # швидкість гравця
+        self.speed = 200
 
     def update(self, dt, keys):
-        # керування WASD
+
         dx = 0
         dy = 0
         if keys[pygame.K_w]:
@@ -33,6 +21,6 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_d]:
             dx = self.speed * dt
 
-        # оновлення позиції
+        # Обновление позиции игрока
         self.rect.x += dx
         self.rect.y += dy
